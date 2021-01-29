c987e2f067b6a2407c267a8de6dc735cd43bbc2a
Sidhant Chopra <sid.chopra@monash.edu>
2021-01-29 19:10:26 +1100

    Add files via upload

--- make_enigma_forestplots_figure1.R
+++ make_enigma_forestplots_figure1.R
@@ -1,6 +1,8 @@
 make_enigma_forestplots_figure1 <- function(data, 
                                             bonferoni=TRUE,
-                                            number.of.comparisions = 156) {
+                                            number.of.comparisions = 160, 
+                                            ylab=F,
+                                            xlab=F) {
   
   
   data$Site <- as.factor(data$Site)
@@ -11,20 +13,21 @@ make_enigma_forestplots_figure1 <- function(data,
   
   i<-1
   for (ii in 11:dim(data)[2]) {
-    f.vect[i,1] <- colnames(data)[ii]
-    tmpvar <- as.vector(unlist(data[ii]))
-    temp <- Anova(lm(tmpvar ~ data$Site + data$Group + data$ICV.c + data$sex  + data$onset.c + data$duration.c + data$age.c), type=3)
-    f.vect[i,2] <- temp[[3]][[3]][[1]]
-    f.vect[i,3] <- temp[[4]][[3]][[1]]
-    fit <- lm(tmpvar ~ data$Site + data$Group + data$ICV.c + data$sex  + data$onset.c + data$duration.c + data$age.c)
-    em <- as.data.frame(emmeans(fit, ~ Group))
-    meandiff <- em[2,2] - em[1,2]
-    SE <- summary(fit)$coef[11,2]
-    f.vect[i,4] <- meandiff/(sqrt(510)*SE) #d
-    f.vect[i,5] <- meandiff/((sqrt(510)*SE)/sqrt(510)) #t
-    temp <- psych::cohen.d.ci(d = f.vect[i,4], n1 = 248, n2 = 262, alpha=.05)
-    f.vect[i,6] <- temp[1,1]
-    f.vect[i,7] <- temp[1,3]
+    f.vect[i,1] <- colnames(dataset)[ii]
+    tmpvar <- as.vector(unlist(dataset[ii]))
+    temp <- Anova(lm(tmpvar ~ dataset$Site + dataset$Group + dataset$ICV.c + dataset$sex  + dataset$onset.c + dataset$duration.c + dataset$age.c), type=3)
+    
+    #new method 29/02/2021
+    p <- temp$`Pr(>F)`[3]
+    f <- temp$`F value`[3]
+    t <- sqrt(f)
+    d <- (2*t)/sqrt(494)
+    f.vect[i,2] <- d
+    temp <- psych::cohen.d.ci(d = f.vect[i,2], n1 = 248, n2 = 262, alpha=.05)
+    f.vect[i,3] <- temp[1,1]
+    f.vect[i,4] <- temp[1,3]
+    f.vect[i,5] <- p
+  
     i<-i+1
   }
   
@@ -52,26 +55,42 @@ make_enigma_forestplots_figure1 <- function(data,
     
     f.vect$roi <- factor(f.vect$roi, levels = f.vect$roi)
     
-    #Only display ROIs > 0.05 Bonferoni corrected p (156 comparisions)
+    #Only display ROIs > 0.05 Bonferoni corrected p (160 comparisions)
     bf_p = 0.05/number.of.comparisions
     f.vect.ordered.bf <- f.vect.ordered[which(f.vect.ordered$p < bf_p),]
     
     
-    ggplot(data=f.vect.ordered.bf, aes(x=roi, y=cohens.d, ymin=ci.l, ymax=ci.u)) +
+    plot <- ggplot(data=f.vect.ordered.bf, aes(x=roi, y=cohens.d, ymin=ci.l, ymax=ci.u)) +
       geom_pointrange() + 
       geom_hline(yintercept=0, lty=2) +  # add a dotted line at x=1 after flip
-      coord_flip() +  # flip coordinates (puts labels on y axis)
-      xlab("ROI") + ylab("Cohens D (95% CI)") +
-      theme_bw()
+      coord_flip() +   ylab("Cohens D (95% CI)") + # flip coordinates (puts labels on y axis)
+      xlab("ROI") + theme_bw() + scale_y_continuous(limits = c(-0.1,3)) + theme(text = element_text(size=15))
+    
+    if (xlab == F){plot = plot +  theme(text = element_text(size=15), 
+                                        axis.title.x=element_blank())} 
+    
+    
+    if (ylab == F){plot = plot +  theme(text = element_text(size=15), 
+                                        axis.title.y=element_blank())} 
+    
   } else {
     
     ggplot(data=f.vect.ordered, aes(x=roi, y=cohens.d, ymin=ci.l, ymax=ci.u)) +
       geom_pointrange() + 
       geom_hline(yintercept=0, lty=2) +  # add a dotted line at x=1 after flip
       coord_flip() +  # flip coordinates (puts labels on y axis)
-      xlab("ROI") + ylab("Cohens D (95% CI)") +
-      theme_bw()
+      xlab("ROI") +     ylab("Cohens D (95% CI)") + #
+      theme_bw()  + scale_y_continuous(limits = c(-0.1,3)) + theme(text = element_text(size=15))
+    
+    
+    if (xlab == F){plot = plot +  theme(text = element_text(size=15), 
+                                        axis.title.x=element_blank())} 
+    
+    if (ylab == F){plot = plot +  theme(text = element_text(size=15), 
+                                        axis.title.y=element_blank())} 
+    
   }
   
+  return(plot)
   
 }
